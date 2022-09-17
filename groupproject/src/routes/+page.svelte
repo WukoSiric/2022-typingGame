554317ebb6807045d00d4a6448bd20eff85bfe15
yifan117 <yifan.qiu17@gmail.com>
2022-09-17 12:29:37 +0930

    checkpoint

--- groupproject/src/routes/+page.svelte
+++ groupproject/src/routes/+page.svelte
@@ -1,34 +1,53 @@
-<script>
+<script lang="ts">
+    function hexToRgb(hex: string) {
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result ? {
+    r: parseInt(result[1], 16),
+    g: parseInt(result[2], 16),
+    b: parseInt(result[3], 16)
+  } :  {
+    r: parseInt("0"),
+    g: parseInt("0"),
+    b: parseInt("0")
+  };
+}
+
+let sidebarItemsinitial = [
+    {color: 'f7650a', href: 'youtube.com', data: '&nbsp;Home&nbsp;', text: '&nbsp;Home&nbsp;', rgb: {r:0,g:0,b:0}},
+    {color: 'ff6493', href: 'youtube.com', data: '&nbsp;About&nbsp;', text: '&nbsp;About&nbsp;', rgb: {r:0,g:0,b:0}},
+    {color: '0ef70a', href: 'youtube.com', data: '&nbsp;Our&nbsp;Projects&nbsp;', text: '&nbsp;Our&nbsp;Projects&nbsp;', rgb: {r:0,g:0,b:0}},
+    {color: 'eb170a', href: 'youtube.com', data: '&nbsp;Hire&nbsp;Us&nbsp;', text: '&nbsp;Hire&nbsp;Us&nbsp;', rgb: {r:0,g:0,b:0}}
+];
+
+let sidebarItems = sidebarItemsinitial.map(item => {
+    item.rgb = hexToRgb(item.color);
+    return item
+})
 </script>
 
 <title>Home Page</title>
 
 <div class="navContainer">
     <ul>
-        <li style="--clr:#f7650a">
-            <a href="youtube.com" data-text="&nbsp;Home&nbsp;">&nbsp;Home&nbsp</a>
-        </li>
-        
-        <li style="--clr:#ff6493">
-            <a href="youtube.com" data-text="&nbsp;About Us&nbsp;">&nbsp;About Us&nbsp</a>
-        </li>
-
-        <li style="--clr:#0ef70a">
-            <a href="youtube.com" data-text="&nbsp;Projects&nbsp;">&nbsp;Projects&nbsp</a>
-        </li>
-
-        <li style="--clr:#eb170a">
-            <a href="youtube.com" data-text="&nbsp;Hire Us&nbsp;">&nbsp;Hire Us&nbsp</a>
-        </li>
+        <!-- <div class="navWord">
+            <li style="--clr:#; background-color: rgba({hexToRgb("f7650a").r}, {hexToRgb("f7650a").g}, {hexToRgb("f7650a").b}, 0.5);"> 
+                <a href="youtube.com" data-text=""></a>
+            </li>
+        </div> -->
+        {#each sidebarItems as item}
+            <li style="--clr:#{item.color}">
+                <a href="{item.href}" data-text="{item.data}">{@html {item.text}}</a>
+            </li>
+        {/each}
     </ul>
 </div>
 
 <style>
     * {
-        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
         margin: 0;
         padding: 0;
         box-sizing: border-box;
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
     }
 
     .navContainer {
@@ -36,31 +55,47 @@
         justify-content: center;
         align-items: center;
         min-height: 100vh;
-        background-color: #3b2a27;
-        min-width: 100vw;
+        max-width: max-content;
+        background-color: #000;
+        padding: 20px;
+        
+    }
+
+    .navWord {
+        justify-content: center;
+        min-width: 100%;
+        
     }
 
     ul {
         position: relative;
         display: flex;
-        flex-direction: column;
+        min-height: 100vh;
+        min-width: 100%;
         gap: 30px;
+        justify-content: space-evenly;
+        flex-direction: column;
     }
 
     ul li {
         position: relative;
         list-style: none;
+        min-width: 100%;
+        display: flex;
     }
 
     ul li a {
         position: relative;
-        font-size: 5em;
+        font-size: 2em;
+        width: 100%;
         font-weight: bold;
         text-decoration: none;
         line-height: 1em;
         letter-spacing: 2px;
         text-transform: uppercase;
         color: transparent;
+        min-width: 100%;
+        flex: 1;
         -webkit-text-stroke: 1px rgba(225, 225, 225, 0.5);
     }
 
@@ -69,14 +104,20 @@
         position: absolute;
         color: white;
         width: 0;
+        flex: 1;
+        word-wrap: initial;
         overflow: hidden;
         transition: 1s;
         border-right: 8px solid var(--clr);
         -webkit-text-stroke: 1px var(--clr);
+        flex: 1;
     }
 
     ul li a:hover::before {
         width: 100%;
-        filter: drop-shadow(0 0 25px var(--clr));
+        flex: 1;
+        background: var(--clr);
+        opacity: 30%;
     }
+
 </style>
---
+++ groupproject/src/routes/our-projects/+page.svelte
@@ -0,0 +1,140 @@
+<script lang='ts'>
+	import { each } from "svelte/internal";
+
+let resources = [
+    {link: 'https://kit.svelte.dev/', styling: 'background-image: url(https://miro.medium.com/max/1000/1*joB4ddC9_-3okvy0r8_CPQ.png); background-color: #ff3c00', name: 'Introduction to SvelteKit'}
+];
+
+let descriptions = [
+    {text: 'asd'}
+];
+
+</script>
+
+<title>Svelte Materials</title>
+
+<body>
+    <div class="cards-container">
+        {#each resources as resource}
+            <a href="{resource.link}" target="_blank" class="card">
+                <div class="card-background" style="{resource.styling}"></div>
+                <div class="card-content">
+                    <p>{resource.name}</p>
+                </div>
+            </a>
+        {/each}
+
+        <div class="description-container"></div>
+        {#each descriptions as description}
+        <div class="card">
+            <div class="description-background" style="#fff"></div>
+            <div class="description-background">
+                    <p>{description.text}</p>
+                </div>
+            </div>
+        {/each}
+    </div>
+</body>
+
+<style>
+
+    body {
+        padding: 50px;
+        background-color: #131416;
+    }
+
+    .cards-container {
+        display: grid;
+        flex-direction: column;
+        margin: auto;
+        max-width: 1200px;
+        
+        gap: 10px;
+    }
+
+    .card {
+        position: relative;
+        padding: 10%;
+        background-size: contain;
+    }
+
+    .card-background {
+        background-size: contain;
+        background-repeat: no-repeat;
+        background-position: center;
+        border-radius: 24px;
+        filter: brightness(0.75) saturate(1.2) contrast(0.85);
+        position: absolute;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        top: 0;
+        transform-origin: center;
+        transition: .2s;
+    }
+
+    .description-background {
+        background-size: contain;
+        background-repeat: no-repeat;
+        background-position: center;
+        border-radius: 24px;
+        filter: brightness(0.75) saturate(1.2) contrast(0.85);
+        position: absolute;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        top: 0;
+        transform-origin: center;
+        transition: .2s;
+        background: #fff;
+    }
+
+    .card:hover .card-background {
+        transform: scale(1.05) translateZ(0);
+    }
+
+    .cards-container:hover>.card:not(:hover) .card-background {
+        filter: brightness(0.5) saturate(0) contrast(1.2) blur(20px);
+    }
+
+    @media(min-width: 960px) {
+        .cards-container {
+            grid-template-columns: repeat(2, 2fr);
+        }
+    }
+
+    .card-content {
+        padding: 24px;
+        position: relative;
+        display: flex;
+        font-family: Arial, Helvetica, sans-serif;
+        max-height: 100%;
+        max-width: 100%;
+        align-items: center;
+        justify-content: center;
+        text-align: center;
+    }
+
+    p {
+        color: #fff;
+        font-size: 2vw;
+        display: flex;
+        text-transform: uppercase;
+        font-weight: 700;
+        letter-spacing: 0.1em;
+        text-shadow: 0 0 10px rgba(3, 3, 3, 1);
+        position: absolute;
+        text-align: center;
+        border: 5px solid #fff;
+        
+    }
+
+    .cards-container:hover>.card:not(:hover) p {
+        opacity: 0;
+    }
+
+    .card:hover p {
+        opacity: 100;
+    }
+
+</style>
