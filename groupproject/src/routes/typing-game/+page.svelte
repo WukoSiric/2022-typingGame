f18eeeba89bc4f8339d719b9b658672b7f4e436c
Luke Stainer <lstainer2002@gmail.com>
2022-09-18 13:23:09 +0930

    new_game() on onmount

--- groupproject/src/routes/typing-game/+page.svelte
+++ groupproject/src/routes/typing-game/+page.svelte
@@ -1,18 +1,19 @@
 <script lang="ts">
-	// import test from "@playwright/test";
-	// import { get_all_dirty_from_scope } from "svelte/internal";
-	// import type { Timer } from "./typing_functions/timer";
-
-    let index: number = 0;
-    let letter_sequence: string; //Show current letter with letter_sequence[index]
-    let user_input: string; //Input binds into this
+    import { onMount } from 'svelte';
+    
+    let index: number;
+    let letter_sequence: string; //Show current letter for user to type with letter_sequence[index]
+    let user_input: string; //Input binds into this with keydow
+    let game_won: boolean;
     const sequence_length = 20;
 
     $: score = 0; //Display this to user
     let round_time: number = 3000;
 
-    function start_game() {
-        let index: number = 0; 
+    function new_game() {
+        index = 0; 
+        game_won = false;
+        user_input = "";
         generateLetters();
     }
 
@@ -24,7 +25,16 @@
             new_sequence += possible_chars.charAt(Math.floor(Math.random() * chars_length));
         }
         letter_sequence = new_sequence;
-        console.log(letter_sequence);
+    }
+
+    // Will be called when doing input -> on:keydown={handle_input}
+    function handle_input(letter: string): void {
+        // Doesnt account for time yet
+        if (is_correct_letter(letter, index)) {
+            score++; 
+        }
+        console.log(score);
+        index++;
     }
 
     function is_correct_letter(letter: string, index: number): boolean {
@@ -35,27 +45,7 @@
         return false;
     }
 
-    // Will be called for on:keydown={handle_input}
-    function handle_input(letter: string): void {
-        // Only updating the score
-        // Need to add handling time!
-        if (is_correct_letter(letter, index)) {
-            score++; 
-        }
-        console.log(score);
-        index++;
-    }
-
-    setTimeout(driver, 1000);
-
-    function driver() {
-        letter_sequence = 'abcdef';
-        handle_input('a');
-        handle_input('b');
-        handle_input('c');
-        handle_input('a');
-        handle_input('b');
-        handle_input('c');
-    }
-    generateLetters();
+    onMount(() => {
+        new_game();
+    });
 </script>   
